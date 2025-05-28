28f2dca8dff7a59bdf8704aa0848867bc4106100
kuntalkhan <kuntal.khan2004@gmail.com>
2025-05-28 15:36:22 +0530

    added js functions

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ const search_input = document.querySelector('.search_input');
 const search_btn = document.querySelector('.search_button');
 const frame = document.querySelector('.browser_content');
 
-search_btn.addEventListener('click', () => {
-    console.log(search_input.value.trim());
-});
+const search_function = ()=> {
+    const url = search_input.value.trim();
+    const re =/www\.[a-zA-Z0-9-]+\.[a-z]{2,}/g;
+    if (re.test(url)){
+        if(!url.startsWith('https://')) {
+            frame.innerHTML =`
+            <h2> ${url}</h2>
+            <iframe src="https://${url}" frameborder="0" ></iframe>
+            `
+        } else{
+            frame.innerHTML =`
+            <h2> ${url}</h2>
+            <iframe src="${url}" frameborder="0" ></iframe>
+            `
+        }
+    } else {
+        window.open(`https://www.google.com/search?q=${url}`, '_blank');
+        frame.innerHTML = '';
+    }
+}
+
+search_btn.addEventListener('click', search_function);
+search_input.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        search_function();
+    }
+}); 
